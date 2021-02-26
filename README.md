7133aaab88b667c2e6d0317186e64a0eb4015ed8
Josh Lin <42842303+jlin-orange@users.noreply.github.com>
2021-02-26 10:15:20 -0500

    README+ finished documentation

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # 7-Layers-Of-Markov
  A simple webapp that generates and displays a 7-layer dip using Markov Chains!
+
+7-layer dips are randomly generated in two parts - their base ingredients + garnishes.
+
+|**Base ingredients**|**Garnishes**|
+| --- | --- |
+| RB, Refried Beans| OL, Olives |
+| SC, Sour cream   | GO, Green onion |
+| SS, Salsa        | JP, Jalapenos |
+| GC, Guacamole    | TM, Tomatoes |
+| SM, Seasoned meat| CH, Cheese |
+| HM, Hummus       | CL, Cilantro |
+| PY, Plain Yogurt | DP, Diced peppers |
+
+<br> A base ingredient and garnish are selected to start and the remaining
+layers are randomly chosen using a Markov chain. Ingredient images
+have been pulled from online and resized courtesy of Wruff Pty Ltd's very 
+useful [batch resizing tool](https://bulkresizephotos.com/en).
+
+## How to set up and run the code~
+You can view this project on your local through the VSCode Extension: [Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer)
+
+1) Clone this repo to your local machine
+2) Open the repo in VSCode
+3) Click on the "Go Live" button in the bottom-right corner of your client
+
+Alternatively, you can visit: http://7-layer-generator.rf.gd/
+
+## Personal Significance 
+I enjoy silly websites^, and I wanted to make something
+simple and fun that would combine personal interests with my 
+current technical know-how. Whenever I cook, I like to 
+look at a variety of recipes to see the difference in technique
+and ingredients used. After learning about Markov chains,
+I realized I could probably mimic this sort of exploration 
+on a website for the greater good (but mostly my personal interest). 
+
+^ - https://theuselessweb.com/sites/ 
+
+## Challenges faced
+I haven't had much experience with Web Development outside of
+Hackathons so this was a valuable opportunity for me to formally
+approach a web project with thorough documentation and thought. 
+JavaScript is still a very new language to me, so it was a challenge
+figuring out how I could implement Markov Chains using JS Objects
+as opposed to the Dictionary structures available in languages like
+Java/Python. I had a fun time scouring Stack Overflow and Google to
+find the resources/answers I needed to make things work!
+Moving forward, I want to explore Javascript libraries and/or frameworks
+to see what they're capable of and how they compare to vanilla Javascript.
+
+
+## Is this a Creative System?
+I believe this is a Creative system according to the characteristics of
+novelty, value, intentionality; this system varies with regards to how
+well it exhibits each of these. The domain of this system is at the 
+intersection of culinary recipes and visual art. 
+
+This is the first time I've worked on a project in this domain and, as far
+as I'm aware, I've yet to see a project so dedicated to the 7-layer dip. 
+Since this project is relatively small in size, I would say that it's value
+is restricted to its aesthetic side. Still, I could see a potential application
+for 7-layer aficionados to spice up their 7-layer dip game by using this to explore
+different combinations/ratios of ingredients. I weighted the transition matrix 
+for the Markov chains according to patterns I noticed in the most visible recipes 
+I could find online. Assuming that those recipes are visible because they are 
+popular and/or well-received, this system is intended to produce dips that are
+both novel and preferable for consumption.  
+
+## Sources
+[Appending Image in JavaScript](https://www.codegrepper.com/code-examples/delphi/how+to+append+image+in+javascript) <br>
+[Random selection from a weighted array](https://stackoverflow.com/questions/43566019/how-to-choose-a-weighted-random-array-element-in-javascript) <br>
+[Button "pressed-down" Animation](https://www.w3schools.com/css/css3_buttons.asp)
--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
 // Constants
+NUM_BASE_INGREDIENTS = 4;
+NUM_GARNISHES = 3;
+
+STARTER_BASE = "RB";
+STARTER_GARNISH = "CH";
+
 TRANSITION_MATRIX = {
-    // Base Ingredients
+    // All Base Ingredients
     "RB": {"RB": 0.0, "SC": 0.35, "SS": 0.075, "GC": 0.075, "SM": 0.35, "HM": 0.075, "PY": 0.075}, 
     "SC": {"RB": 0.025, "SC": 0.0, "SS": 0.025, "GC": 0.65, "SM": 0.25, "HM": 0.025, "PY": 0.025},
     "SS": {"RB": 0.17, "SC": 0.17, "SS": 0.0, "GC": 0.17, "SM": 0.17, "HM": 0.16, "PY": 0.16},
@@ -9,7 +15,7 @@ TRANSITION_MATRIX = {
     "HM": {"RB": 0.16, "SC": 0.16, "SS": 0.16, "GC": 0.16, "SM": 0.16, "HM": 0.0, "PY": 0.2},
     "PY": {"RB": 0.16, "SC": 0.16, "SS": 0.16, "GC": 0.16, "SM": 0.16, "HM": 0.2, "PY": 0.0},
     
-    // Garnishes
+    // All Garnishes
     "OL": {"OL": 0.0, "GO": 0.17, "JP": 0.17, "TM": 0.17, "CH": 0.17, "CL": 0.16, "DP": 0.16}, 
     "GO": {"OL": 0.16, "GO": 0.0, "JP": 0.17, "TM": 0.17, "CH": 0.17, "CL": 0.17, "DP": 0.16},
     "JP": {"OL": 0.16, "GO": 0.16, "JP": 0.0, "TM": 0.17, "CH": 0.17, "CL": 0.17, "DP": 0.17},
@@ -19,60 +25,81 @@ TRANSITION_MATRIX = {
     "DP": {"OL": 0.17, "GO": 0.17, "JP": 0.17, "TM": 0.17, "CH": 0.16, "CL": 0.16, "DP": 0.0}
 };
 
-NUM_BASE_INGREDIENTS = 4;
-NUM_GARNISHES = 3;
 
-STARTER_BASE = "RB";
-STARTER_GARNISH = "CH";
-
-// Wrapper function
+// Wrapper function to run generate_dip() whenever the HTML button is clicked
 function main() {
-    generate_dip(TRANSITION_MATRIX, STARTER_BASE, STARTER_GARNISH);
+    dip_arr = [];
+    dip_arr = generate_dip_array(TRANSITION_MATRIX, STARTER_BASE, STARTER_GARNISH);
+    display_array_content(dip_arr);
 }
 
-function generate_dip(transition_probs, first_base, first_garnish) {
-    // TODO: clear current dip images
-    // clear_display();
+/**
+ * Populates an array with strings representing each layer of a 7-layer dip
+ * @param {Object} transition_probs The Markov transition matrix
+ * @param {String} first_base The first base ingredient to be added
+ * @param {String} first_garnish The first garnish to be added
+ * @return {Array} layer_arr The array populated with all necessary contents
+ */
+function generate_dip_array(transition_probs, first_base, first_garnish) {
+    layer_arr = [];
 
-    dip_arr = [];
+    // Tracks the most recent dip layer
+    prev_layer = first_base
 
-    prev_layer = first_base // local var to store the previous layer of dip, also sets a default layer
     // Generate base ingredients first
     for (i = 0; i < NUM_BASE_INGREDIENTS; i++) {
-        dip_arr.push(prev_layer);
+        layer_arr.push(prev_layer);
         prev_layer = get_ingredient(transition_probs, prev_layer);
     }
 
     // Generate garnishes second 
     prev_layer = first_garnish;
     for (i = 0; i < NUM_GARNISHES; i++) {
-        dip_arr.push(prev_layer);
+        layer_arr.push(prev_layer);
         prev_layer = get_ingredient(transition_probs, prev_layer)
     }
 
-    display_dip(dip_arr);
+    return layer_arr;
 }
 
-// insert pictures according to what's in the dip array
-// depending on what index i is: insert photo in the corresponding DIV
-function display_dip(layer_arr) {
-    for (i = 0; i < layer_arr.length; i++) { 
-        var new_img = document.createElement("img");
-        new_img.src = `imgs/${layer_arr[i]}.jpg`;
 
-        var dip_layer = document.getElementById(`layer_${i+1}`);
+/**
+ * Display contents of an array as images
+ * @param {Array} target_arr The array with content of interest
+ */
+function display_array_content(target_arr) {
+    for (i = 0; i < target_arr.length; i++) { 
+        var new_img = document.createElement("img");
+        new_img.src = `imgs/${target_arr[i]}.jpg`;
+        new_img.alt = "HTML5";
+        new_img.style = "width:150px;height:50px";
+
+        var target_element = document.getElementById(`layer_${i+1}`);
         
-        if (dip_layer.childElementCount > 0) {dip_layer.removeChild(dip_layer.firstElementChild);} // clears out old pictures!
-        dip_layer.appendChild(new_img);
+        // Remove old elements as to avoid stacking past images
+        if (target_element.childElementCount > 0) {target_element.removeChild(target_element.firstElementChild);} 
+        
+        target_element.appendChild(new_img);
     }
 }
 
-// return a new ingredient according to transition probabilities
+/**
+ * Given an ingredient, gets a new ingredient according to predefined transition probabilities
+ * @param {Object} transition_probs the Markov transition matrix
+ * @param {String} prev_ingredient the previous ingredient selected
+ * @return {String} the new ingredient
+ */
 function get_ingredient(transition_probs, prev_ingredient) {
     return weighted_random(Object.keys(transition_probs[prev_ingredient]), Object.values(transition_probs[prev_ingredient]));
 }
 
-// src: https://stackoverflow.com/questions/43566019/how-to-choose-a-weighted-random-array-element-in-javascript
+/**
+ * Chooses an item from an array given weights 
+ * @param {Array} items Items to be chosen among 
+ * @param {Array} weights Probabilities for an item to be selected  
+ * @return {*} An item from the items array 
+ * Code taken from: https://stackoverflow.com/questions/43566019/how-to-choose-a-weighted-random-array-element-in-javascript
+ */
 function weighted_random(items, weights) {
     var i;
 
